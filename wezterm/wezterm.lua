3d3be874a8a339d955d78792b763e575ee379681
sreejith <sreejithsree956266@gmail.com>
2024-12-21 23:41:04 +0530

    wezterm cofig added

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,193 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+config.default_prog = { "C:\\Program Files\\Git\\bin\\bash.exd" }
+
+-- For example, changing the color scheme:
+config.color_scheme = "Catppuccin Mocha"
+-- config.color_scheme = "nord"
+-- config.font = wezterm.font("JetBrains Mono NL")
+config.font = wezterm.font("JetBrains Mono SemiBold")
+config.font_size = 13.8
+
+config.window_decorations = "RESIZE"
+
+-- tmux
+config.leader = { key = "q", mods = "ALT", timeout_milliseconds = 2000 }
+config.keys = {
+	{
+		mods = "LEADER",
+		key = "c",
+		action = wezterm.action.SpawnTab("CurrentPaneDomain"),
+	},
+	{
+		mods = "LEADER",
+		key = "x",
+		action = wezterm.action.CloseCurrentPane({ confirm = true }),
+	},
+	{
+		mods = "LEADER",
+		key = "b",
+		action = wezterm.action.ActivateTabRelative(-1),
+	},
+	{
+		mods = "LEADER",
+		key = "n",
+		action = wezterm.action.ActivateTabRelative(1),
+	},
+	{
+		mods = "LEADER",
+		key = "/",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		mods = "LEADER",
+		key = "-",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		mods = "LEADER",
+		key = "h",
+		action = wezterm.action.ActivatePaneDirection("Left"),
+	},
+	{
+		mods = "LEADER",
+		key = "j",
+		action = wezterm.action.ActivatePaneDirection("Down"),
+	},
+	{
+		mods = "LEADER",
+		key = "k",
+		action = wezterm.action.ActivatePaneDirection("Up"),
+	},
+	{
+		mods = "LEADER",
+		key = "l",
+		action = wezterm.action.ActivatePaneDirection("Right"),
+	},
+	{
+		mods = "LEADER",
+		key = "LeftArrow",
+		action = wezterm.action.AdjustPaneSize({ "Left", 5 }),
+	},
+	{
+		mods = "LEADER",
+		key = "RightArrow",
+		action = wezterm.action.AdjustPaneSize({ "Right", 5 }),
+	},
+	{
+		mods = "LEADER",
+		key = "DownArrow",
+		action = wezterm.action.AdjustPaneSize({ "Down", 5 }),
+	},
+	{
+		mods = "LEADER",
+		key = "UpArrow",
+		action = wezterm.action.AdjustPaneSize({ "Up", 5 }),
+	},
+}
+
+for i = 0, 9 do
+	-- leader + number to activate that tab
+	table.insert(config.keys, {
+		key = tostring(i),
+		mods = "LEADER",
+		action = wezterm.action.ActivateTab(i),
+	})
+end
+
+-- tab bar
+config.hide_tab_bar_if_only_one_tab = false
+config.tab_bar_at_bottom = true
+config.use_fancy_tab_bar = false
+config.tab_and_split_indices_are_zero_based = true
+
+-- Helper function to extract current working directory for Windows
+function get_current_working_dir(tab)
+	local cwd_uri = tab.active_pane.current_working_dir
+	if cwd_uri then
+		-- Remove the 'file:///' prefix and decode any percent-encoded characters
+		local cwd = cwd_uri:sub(8):gsub("%%(%x%x)", function(hex)
+			return string.char(tonumber(hex, 16))
+		end)
+
+		-- For Windows, extract the last part of the path after the last backslash
+		return cwd:match("([^\\]+)$") or cwd
+	end
+	return "[No CWD]"
+end
+
+-- Helper function to get the active process name in the pane
+function get_process(tab)
+	local process_name = tab.active_pane.foreground_process_name
+	if process_name then
+		return process_name:match("([^/\\]+)$") -- Extract the process name from the full path
+	end
+	return nil
+end
+
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+	local has_unseen_output = false
+	if not tab.is_active then
+		for _, pane in ipairs(tab.panes) do
+			if pane.has_unseen_output then
+				has_unseen_output = true
+				break
+			end
+		end
+	end
+
+	-- Get current directory and process
+	local cwd = wezterm.format({ { Text = get_current_working_dir(tab) } })
+	local process = get_process(tab)
+
+	-- Construct the tab title
+	local title = process and string.format(" %s (%s) ", process, cwd) or " [?] "
+
+	-- If the tab has unseen output, change the title color
+	if has_unseen_output then
+		return {
+			{ Foreground = { Color = "#28719c" } },
+			{ Text = title },
+		}
+	end
+
+	return {
+		{ Text = title },
+	}
+end)
+
+-- tmux status
+wezterm.on("update-right-status", function(window, _)
+	local SOLID_LEFT_ARROW = ""
+	local ARROW_FOREGROUND = { Foreground = { Color = "#c6a0f6" } }
+	local prefix = ""
+
+	if window:leader_is_active() then
+		prefix = " " .. utf8.char(0x1f30a) -- ocean wave
+		SOLID_LEFT_ARROW = utf8.char(0xe0b2)
+	end
+
+	if window:active_tab():tab_id() ~= 0 then
+		ARROW_FOREGROUND = { Foreground = { Color = "#1e2030" } }
+	end -- arrow color based on if tab is first pane
+
+	window:set_left_status(wezterm.format({
+		{ Background = { Color = "#b7bdf8" } },
+		{ Text = prefix },
+		ARROW_FOREGROUND,
+		{ Text = SOLID_LEFT_ARROW },
+	}))
+end)
+
+-- and finally, return the configuration to wezterm
+return config
